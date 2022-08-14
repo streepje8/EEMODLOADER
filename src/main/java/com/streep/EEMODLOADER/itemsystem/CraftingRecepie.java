b3e1cb1c2a354f288491455bd064bac5eea84061
streepje8 <wessel@wezzel.nl>
2022-08-14 19:16:46 +0200

    Safe crafts to JSON

--- src/main/java/com/streep/EEMODLOADER/itemsystem/CraftingRecepie.java
+++ src/main/java/com/streep/EEMODLOADER/itemsystem/CraftingRecepie.java
@@ -1,5 +1,8 @@
 package com.streep.EEMODLOADER.itemsystem;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class CraftingRecepie {
 
 	public boolean shapeless = false;
@@ -15,6 +18,27 @@ public class CraftingRecepie {
 
 	public String getName() {
 		return name;
+	}
+	
+	public JSONObject toJSON() {
+		JSONObject result = new JSONObject();
+		result.put("Name", name);
+		result.put("Shapeless", shapeless);
+		result.put("Result", this.result.toJsonObject());
+		JSONArray ingredients = new JSONArray();
+		for(int y = 0; y < 3; y++) {
+			for(int x = 0; x < 3; x++) {
+				if(this.items[x][y] != null) {
+					JSONObject ingredient = new JSONObject();
+					ingredient.put("X", x);
+					ingredient.put("Y", y);
+					ingredient.put("Item", this.items[x][y].toJsonObject());
+					ingredients.put(ingredient);
+				}
+			}
+		}
+		result.put("Ingredients", ingredients);
+		return result;
 	}
 	
 }
